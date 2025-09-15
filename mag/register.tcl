818d73c566cbb02a39f75663954803d5d3ad08a7
Mike Bell <mdb036@gmail.com>
2025-09-15 17:49:52 +0100

    Add extra taps to fix DRC

--- mag/register.tcl
+++ mag/register.tcl
@@ -3,23 +3,38 @@ set ROW_HEIGHT 544
 set ROW_HALF_HEIGHT 272
 
 # Instantiate the cells
-for {set i 0} {$i < $REPEATS} {incr i} {
+for {set i 0} {$i < 2} {incr i} {
     box [expr $i * 736] $ROW_HALF_HEIGHT [expr $i * 736] $ROW_HALF_HEIGHT
     getcell sky130_fd_sc_hd__dfxtp_1
     box [expr $i * 736] 0 [expr $i * 736] 0
     getcell sky130_fd_sc_hd__dfxtp_1 180
 }
-box [expr 736 * $REPEATS] $ROW_HALF_HEIGHT [expr 736 * $REPEATS] $ROW_HALF_HEIGHT
+box [expr 736 * 2] $ROW_HALF_HEIGHT [expr 736 * 2] $ROW_HALF_HEIGHT
 getcell sky130_fd_sc_hd__tapvpwrvgnd_1
-box [expr 736 * $REPEATS] 0 [expr 736 * $REPEATS] 0
+box [expr 736 * 2] 0 [expr 736 * 2] 0
+getcell sky130_fd_sc_hd__tapvpwrvgnd_1 v
+for {set i 2} {$i < 4} {incr i} {
+    box [expr $i * 736 + 46] $ROW_HALF_HEIGHT [expr $i * 736 + 46] $ROW_HALF_HEIGHT
+    getcell sky130_fd_sc_hd__dfxtp_1
+    box [expr $i * 736 + 46] 0 [expr $i * 736 + 46] 0
+    getcell sky130_fd_sc_hd__dfxtp_1 180
+}
+
+box [expr 736 * $REPEATS + 46] $ROW_HALF_HEIGHT [expr 736 * $REPEATS + 46] $ROW_HALF_HEIGHT
+getcell sky130_fd_sc_hd__tapvpwrvgnd_1
+box [expr 736 * $REPEATS + 46] 0 [expr 736 * $REPEATS + 46] 0
 getcell sky130_fd_sc_hd__tapvpwrvgnd_1 v
 
 box -276 $ROW_HALF_HEIGHT 0 $ROW_HALF_HEIGHT
 getcell sky130_fd_sc_hd__clkbuf_4
-box -414 $ROW_HALF_HEIGHT -276 $ROW_HALF_HEIGHT
+box -322 $ROW_HALF_HEIGHT 0 $ROW_HALF_HEIGHT
+getcell sky130_fd_sc_hd__tapvpwrvgnd_1
+box -460 $ROW_HALF_HEIGHT -322 $ROW_HALF_HEIGHT
 getcell sky130_fd_sc_hd__decap_3
 box -414 0 0 0
 getcell sky130_fd_sc_hd__mux2_2 180
+box -460 0 -414 0
+getcell sky130_fd_sc_hd__tapvpwrvgnd_1 v
 
 # Clk wiring
 box -234 360 -200 394
@@ -36,22 +51,22 @@ paint viali
 
 box -10 369.5 9 430.5
 paint locali
-box 12 487 [expr 736 * $REPEATS - 11] 503
+box 12 487 [expr 736 * $REPEATS - 11 + 46] 503
 paint metal1
-box 699 41 [expr 736 * $REPEATS - 11] 57
+box 699 41 [expr 736 * $REPEATS - 11 + 46] 57
 paint metal1
-box [expr 736 * $REPEATS - 40] 458 [expr 736 * $REPEATS - 11] 503
+box [expr 736 * $REPEATS - 40 + 46] 458 [expr 736 * $REPEATS - 11 + 46] 503
 paint metal1
-box [expr 736 * $REPEATS - 40] 464 [expr 736 * $REPEATS - 11] 497
+box [expr 736 * $REPEATS - 40 + 46] 464 [expr 736 * $REPEATS - 11 + 46] 497
 paint via1
-box [expr 736 * $REPEATS - 40] 120 [expr 736 * $REPEATS - 11] 153
+box [expr 736 * $REPEATS - 40 + 46] 120 [expr 736 * $REPEATS - 11 + 46] 153
 paint via1
-box [expr 736 * $REPEATS - 40] 117 [expr 736 * $REPEATS - 11] 500
+box [expr 736 * $REPEATS - 40 + 46] 117 [expr 736 * $REPEATS - 11 + 46] 500
 paint metal2
-box [expr 736 * $REPEATS - 40] 57 [expr 736 * $REPEATS - 11] 160
+box [expr 736 * $REPEATS - 40 + 46] 57 [expr 736 * $REPEATS - 11 + 46] 160
 paint metal1
 
-for {set i 0} {$i < $REPEATS} {incr i} {
+for {set i 0} {$i < 2} {incr i} {
     box [expr $i * 736 + 702] 120 [expr $i * 736 + 721] 154
     paint viali
     box [expr $i * 736 + 699] 57 [expr $i * 736 + 724] 160
@@ -63,9 +78,21 @@ for {set i 0} {$i < $REPEATS} {incr i} {
     paint metal1
 }
 
+for {set i 2} {$i < 4} {incr i} {
+    box [expr $i * 736 + 702 + 46] 120 [expr $i * 736 + 721 + 46] 154
+    paint viali
+    box [expr $i * 736 + 699 + 46] 57 [expr $i * 736 + 724 + 46] 160
+    paint metal1
+
+    box [expr $i * 736 + 15 + 46] 390 [expr $i * 736 + 34 + 46] 424
+    paint viali
+    box [expr $i * 736 + 12 + 46] 384 [expr $i * 736 + 37 + 46] 487
+    paint metal1
+}
+
 
 # Data wiring
-for {set i 0} {$i < [expr $REPEATS - 1]} {incr i} {
+set i 0
     box [expr $i * 736 + 696] 315 [expr $i * 736 + 713] 332
     paint viali
     box [expr ($i + 1) * 736 + 143] 381 [expr ($i + 1) * 736 + 160] 398
@@ -74,7 +101,7 @@ for {set i 0} {$i < [expr $REPEATS - 1]} {incr i} {
     paint metal1
 
     # Intentionally add some extra capacitance to improve hold timing
-    box [expr $i * 736 + 250] 312 [expr ($i + 1) * 736 + 200] 364
+    box [expr $i * 736 + 450] 312 [expr ($i + 1) * 736 + 200] 364
     paint metal1
 
     box [expr ($i + 1) * 736 + 24] 212 [expr ($i + 1) * 736 + 41] 229
@@ -85,27 +112,73 @@ for {set i 0} {$i < [expr $REPEATS - 1]} {incr i} {
     paint metal1
 
     # Intentionally add some extra capacitance to improve hold timing
-    box [expr $i * 736 + 250] 180 [expr ($i + 1) * 736 + 200] 232
+    box [expr $i * 736 + 450] 180 [expr ($i + 1) * 736 + 200] 232
     paint metal1
-}
 
-box [expr $i * 736 + 696] 315 [expr $i * 736 + 713] 332
+set i 1
+    box [expr $i * 736 + 696] 315 [expr $i * 736 + 713] 332
+    paint viali
+    box [expr ($i + 1) * 736 + 143 + 46] 381 [expr ($i + 1) * 736 + 160 + 46] 398
+    paint viali
+    box [expr ($i + 1) * 736 + 140 + 46] 312 [expr ($i + 1) * 736 + 163 + 46] 404
+    paint metal1
+
+    # Intentionally add some extra capacitance to improve hold timing
+    box [expr $i * 736 + 450 + 46] 312 [expr ($i + 1) * 736 + 200 + 46] 364
+    paint metal1
+
+    box [expr ($i + 1) * 736 + 24 + 46] 212 [expr ($i + 1) * 736 + 41 + 46] 229
+    paint viali
+    box [expr $i * 736 + 575] 146 [expr $i * 736 + 592] 163
+    paint viali
+    box [expr $i * 736 + 572] 140 [expr $i * 736 + 595] 212
+    paint metal1
+
+    # Intentionally add some extra capacitance to improve hold timing
+    box [expr $i * 736 + 450 + 46] 180 [expr ($i + 1) * 736 + 200 + 46] 232
+    paint metal1
+
+set i 2
+    box [expr $i * 736 + 696 + 46] 315 [expr $i * 736 + 713 + 46] 332
+    paint viali
+    box [expr ($i + 1) * 736 + 143 + 46] 381 [expr ($i + 1) * 736 + 160 + 46] 398
+    paint viali
+    box [expr ($i + 1) * 736 + 140 + 46] 312 [expr ($i + 1) * 736 + 163 + 46] 404
+    paint metal1
+
+    # Intentionally add some extra capacitance to improve hold timing
+    box [expr $i * 736 + 450 + 46] 312 [expr ($i + 1) * 736 + 200 + 46] 364
+    paint metal1
+
+    box [expr ($i + 1) * 736 + 24 + 46] 212 [expr ($i + 1) * 736 + 41 + 46] 229
+    paint viali
+    box [expr $i * 736 + 575 + 46] 146 [expr $i * 736 + 592 + 46] 163
+    paint viali
+    box [expr $i * 736 + 572 + 46] 140 [expr $i * 736 + 595 + 46] 212
+    paint metal1
+
+    # Intentionally add some extra capacitance to improve hold timing
+    box [expr $i * 736 + 450 + 46] 180 [expr ($i + 1) * 736 + 200 + 46] 232
+    paint metal1
+set i 3
+
+box [expr $i * 736 + 696 + 46] 315 [expr $i * 736 + 713 + 46] 332
 paint viali
-box [expr $i * 736 + 500] 312 [expr ($i + 1) * 736] 364
+box [expr $i * 736 + 550 + 46] 312 [expr ($i + 1) * 736 + 46] 364
 paint metal1
 
-box [expr $i * 736 + 575] 146 [expr $i * 736 + 592] 163
+box [expr $i * 736 + 575 + 46] 146 [expr $i * 736 + 592 + 46] 163
 paint viali
-box [expr $i * 736 + 500] 180 [expr ($i + 1) * 736] 232
+box [expr $i * 736 + 550 + 46] 180 [expr ($i + 1) * 736 + 46] 232
 paint metal1
-box [expr $i * 736 + 572] 140 [expr $i * 736 + 595] 212
+box [expr $i * 736 + 572 + 46] 140 [expr $i * 736 + 595 + 46] 212
 paint metal1
 
-box [expr $i * 736 + 600] 200 [expr $i * 736 + 626] 226
+box [expr $i * 736 + 600 + 46] 200 [expr $i * 736 + 626 + 46] 226
 paint via1
-box [expr $i * 736 + 590] 192 [expr $i * 736 + 634] 350
+box [expr $i * 736 + 590 + 46] 192 [expr $i * 736 + 634 + 46] 350
 paint metal2
-box [expr $i * 736 + 600] 318 [expr $i * 736 + 626] 344
+box [expr $i * 736 + 600 + 46] 318 [expr $i * 736 + 626 + 46] 344
 paint via1
 
 
